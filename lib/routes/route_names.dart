03f9fe1f4538246f24254c1bd291f169a9a4bf21
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2023-11-18 21:00:46 -0500

    Feature: estructura inicial y configuracion

---
+++ lib/routes/route_names.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:app/routes/routes.dart';
+
+class NavigationServices {
+  NavigationServices(this.context);
+
+  final BuildContext context;
+
+  Future<dynamic> _pushMaterialPageRoute(Widget widget,
+      {bool fullscreenDialog = false}) async {
+    return await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => widget, fullscreenDialog: fullscreenDialog),
+    );
+  }
+
+  void gotoSplashScreen() {
+    Navigator.pushNamedAndRemoveUntil(
+        context, RoutesName.Splash, (Route<dynamic> route) => false);
+  }
+
+  void gotoIntroductionScreen() {
+    Navigator.pushNamedAndRemoveUntil(context, RoutesName.IntroductionScreen,
+            (Route<dynamic> route) => false);
+  }
+}
